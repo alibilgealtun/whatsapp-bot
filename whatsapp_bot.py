5c77a131e2e1a30c13ba9416eaa04553056d676d
Ali Altun <92324915+alibilgealtun@users.noreply.github.com>
2024-07-23 18:24:27 +0300

    finished

--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -6,8 +6,8 @@ import os
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
-
-conn = sqlite3.connect("contacts2.db")
+db_path = "contacts.db"
+conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 # con.commit() need to be done to change in database
 
@@ -28,7 +28,13 @@ def start():
 
 
 
-
+def clear_person():
+    search_box = driver.find_element(By.XPATH, '//div[@aria-label="Search input textbox"]')
+    search_box.click()  # Ensure the search box is focused
+    search_box.clear()  # Attempt to clear the text
+    search_box.send_keys(Keys.CONTROL + "a")  # Select all text
+    search_box.send_keys(Keys.DELETE)  # Delete selected text
+    time.sleep(0.5)  # Small delay to ensure the clearing action is processed
 
 def search_person(contact):
     search_box = driver.find_element(By.XPATH, '//div[@aria-label="Search input textbox"]')
@@ -54,8 +60,6 @@ def find_pdfs():
     return pdf_paths
 
 def send_pdfs():
-    # Click the attachment button
-
 
     pdf_paths = find_pdfs()
     for pdf_path in pdf_paths:
@@ -66,7 +70,8 @@ def send_pdfs():
             # Re-locate the file input element for each upload attempt
             pdf_input = driver.find_element(By.XPATH, '//input[@accept="*"]')
             pdf_input.send_keys(pdf_path)
-            time.sleep(1)  # Wait for the file to be selected
+            time.sleep(3)
+
 
             # Click the send button
             send_button = driver.find_element(By.XPATH, "//span[@data-icon='send']")
@@ -79,27 +84,63 @@ def send_pdfs():
 
 
 
-def fetch_contacts(path):
+def fetch_contacts():
     query = 'SELECT name FROM contacts'
     cursor.execute(query)
     contacts = [row[0] for row in cursor.fetchall()]
     return contacts
 
+
+def delete_contact(contact):
+    try:
+        delete_sql = "DELETE FROM contacts WHERE name = ?"
+        cursor.execute(delete_sql, (contact,))
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+
+
+def isRightPerson(contact):
+    try:
+        # Locate the span inside the div with class "_amie"
+        profile_text = driver.find_element(By.XPATH, '//div[@class="_amie"]//span').text
+        # Compare the profile text with the contact
+        if contact == profile_text:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"An error occurred while verifying the contact: {str(e)}")
+        return False
+
+
 def main():
-    db_path = "contacts2.db"
-    contacts = fetch_contacts(db_path)
+    contacts = fetch_contacts()
+    print(contacts)
     message = """
-        Bu bir deneme mesajıdır. Bot yazıyorum.
+            Hello! We are excited to share our latest furniture catalogs and price lists with you. This month, we have some amazing discounts on our top-quality furniture:
+            Vino Sofa: 27% off
+            Rahat Furniture: 10% off
+            Lucente: 30% off
+            Please find the attached PDFs above for more details. We look forward to your orders and inquiries.
+            Best regards, Halit Altun - VIA Dış Ticaret
+            
     """
 
     start()
     input("ENTERA BAS QR GİRİNCE")
 
     for contact in contacts:
-        search_person(contact)
-        send_message(message)
-        send_pdfs()
-
+        try:
+            clear_person()
+            search_person(contact)
+            if isRightPerson(contact):
+                send_pdfs()
+                time.sleep(2)
+                send_message(message)
+                delete_contact(contact)
+        except Exception:
+            print("Error for contact: ", contact)
 
 main()
 
